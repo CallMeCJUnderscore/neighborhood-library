ec04f9d1a728c3b5b0c9ea28865416961a38f674
CallMeCJUnderscore <monsterboy09@gmail.com>
2023-10-13 15:56:41 -0400

    Initial Commit

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,97 @@
 package com.pluralsight;
 
 public class Book {
+    /*-----------VARIABLES---------------*/
+    private int id;
+    private String isbn;
+    private String title;
+    boolean isCheckedOut;
+    private String checkedOutTo;
+
+    /*------------CONSTRUCTORS-----------*/
+
+    public Book() {
+        id= 0;
+        isbn = "";
+        title = "";
+        isCheckedOut = false;
+        checkedOutTo = "";
+    }
+
+    /*----------GETTERS/SETTERS----------*/
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    /*-------------FUNCTIONS-------------*/
+
+    public void checkOut(String checkedOutTo){
+        if(isCheckedOut){
+            System.out.println("This book was already checked out!");
+        }
+        else{
+            System.out.println("This book was successfully checked out to: " + checkedOutTo);
+            this.checkedOutTo = checkedOutTo;
+            isCheckedOut = true;
+        }
+
+    }
+    public void checkIn(){
+        if(!isCheckedOut){
+            System.out.println("This book isn't checked out!");
+        }
+        else{
+            System.out.println("This book has been checked in.");
+            checkedOutTo = "";
+            isCheckedOut = false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", isCheckedOut=" + isCheckedOut +
+                ", checkedOutTo='" + checkedOutTo + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/pluralsight/Screen.java
+++ src/main/java/com/pluralsight/Screen.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Screen {
+    public static void main(String[] args) {
+        char command;
+        int numOfBooks;
+        Book[] books = new Book[20];
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public static void showAvailableBooks(Book[] books, int numOfBooks){
+        for(int i = 1; i <= numOfBooks; i++){
+
+        }
+    }
+
 }
